5e6a3617ea456c6ab5c2a48de2e2346adf8d410e
gengiskhan3 <ayoubmimoune9@gmail.com>
2024-12-23 17:48:06 +0000

    changed cd to -chdir

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,14 +12,14 @@ resource "aws_vpc" "main" {
 #IG to facilitate communication between VPC and the internet
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
-  
+
   tags = {
     Name = "Main VPC IG"
   }
 }
 
 resource "aws_subnet" "main" {
-  vpc_id = aws_vpc.main.id
+  vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
 
   tags = {
@@ -33,56 +33,56 @@ resource "aws_route_table" "main" {
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main.id
-    }
+  }
 
-    tags = {
-      #Name = "Main VPC Route Table"
-    }
+  tags = {
+    #Name = "Main VPC Route Table"
+  }
 }
 
 resource "aws_route_table_association" "public" {
-    subnet_id = aws_subnet.main.id
-    route_table_id = aws_route_table.main.id
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
 }
 
 resource "aws_security_group" "web_server" {
-  name = "web_server_sg"
+  name        = "web_server_sg"
   description = "Allow inbound traffic on port 80 and 22 for ssh"
-  vpc_id = aws_vpc.main.id
+  vpc_id      = aws_vpc.main.id
 
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-    }
+  }
 
-    tags = {
-      Name = "Web Server Security Group"
-    }
+  tags = {
+    Name = "Web Server Security Group"
+  }
 }
 
 resource "aws_instance" "web_server" {
 
-    ami = var.ami_id
-    instance_type = var.instance_type
-    subnet_id = aws_subnet.main.id
-    vpc_security_group_ids = [aws_security_group.web_server.id]
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.main.id
+  vpc_security_group_ids = [aws_security_group.web_server.id]
 
-    user_data = <<-EOF
+  user_data = <<-EOF
     #!/bin/bash
     sudo yum update -y
     sudo yum install -y docker
@@ -91,14 +91,14 @@ resource "aws_instance" "web_server" {
     sudo docker run -d -p 80:80 ${var.dockerhub_username}/${var.docker_image}:latest
     EOF
 
-    tags = {
-      Name = "Web Server Instance"
-    }
+  tags = {
+    Name = "Web Server Instance"
+  }
 }
 
 resource "aws_eip" "web_server_ip" {
   instance = aws_instance.web_server.id
-  vpc = true
+  vpc      = true
 
   tags = {
     Name = "Web Server Public IP"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,19 @@
 variable "instance_type" {
   description = "EC2 instance type"
-  type = string
+  type        = string
 }
 
 variable "ami_id" {
   description = "AMI ID"
-  type = string
+  type        = string
 }
 
 variable "dockerhub_username" {
   description = "DockerHub username"
-  type = string
+  type        = string
 }
 
 variable "docker_image" {
   description = "Docker image"
-  type = string
+  type        = string
 }
